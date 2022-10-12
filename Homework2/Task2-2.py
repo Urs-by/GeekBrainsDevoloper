8773d18f64b6fbdf81a9b5048a5b2bc10511e764
Artur.by <urs.by@iclood.com>
2022-10-12 21:22:05 +0300

    In Task2 from homework2 add metod2

--- Homework2/Task2-2.py
+++ Homework2/Task2-2.py
@@ -2,10 +2,23 @@
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-n = int(input("Введите число: "))
-# Костыльный метод
-my_list = [1]
+n = int(input("Введите целое число: "))
+# Костыльный метод 1
+list1 = [1]
 for i in range(1, n + 1):
-    my_list.append(i * my_list[i - 1])
-my_list.pop(0)
-print(my_list)
+    list1.append(i * list1[i - 1])
+list1.pop(0)
+print(f"Метод 1: {list1}")
+
+
+# Метод 2
+def multip(number):
+    res = 1
+    for i in range(1, number+1):
+        res = res * i
+    return res
+
+list2 = []
+for i in range(1, n+1):
+    list2.append(multip(i))
+print(f"Метод 2: {list2}")
