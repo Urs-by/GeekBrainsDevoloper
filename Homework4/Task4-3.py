4ffef4ed0122a584c6c82f26ace7e0e513926c3e
Artur.by <urs.by@iclood.com>
2022-10-20 19:55:43 +0300

    Task3 - fixed from homework4 solved

--- Homework4/Task4-3.py
+++ Homework4/Task4-3.py
@@ -2,28 +2,10 @@
 # Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 import random
 
-# n = int(input("Введите длинну массива: "))
-# my_list = [random.randint(1, 100) for i in range(n)]
-# set_my_list = list(set(my_list))
-# print("Исходный массив: ")
-# print(my_list)
-# print("Список неповторяющихся элементов:")
-# print(set_my_list)
-pi=3.14159
-k = 1
-s = 0
-i=0
-
-while round(s, 3) != round(pi, 3):
-
-    if i % 2 == 0:
-        s += 4/k
-
-    else:
-        s -= 4/k
-    i += 1
-    k += 2
-
-print(i)
-
-print(f"Количество итераций равно {i}")
+n = int(input("Введите длинну массива: "))
+my_list = [random.randint(1, 100) for i in range(n)]
+set_my_list = list(set(my_list))
+print("Исходный массив: ")
+print(my_list)
+print("Список неповторяющихся элементов:")
+print(set_my_list)
